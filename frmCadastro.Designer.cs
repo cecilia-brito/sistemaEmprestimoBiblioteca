bd6ad3b7cee020f5391d0395c38a76060924d74c
Cecilia Brito Santos <ceciliabritosantos@gmail.com>
2023-03-05 18:32:28 -0300

    criação telas de login e cadastro

---
+++ frmCadastro.Designer.cs
@@ -0,0 +1,264 @@
+﻿
+namespace sistemaEmprestimoBiblioteca
+{
+    partial class frmCadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelCadastro = new System.Windows.Forms.Panel();
+            this.lbLoginAquiCadastro = new System.Windows.Forms.Label();
+            this.lbOULogin = new System.Windows.Forms.Label();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.lbEmailCadastro = new System.Windows.Forms.Label();
+            this.lbSenhaCadastro = new System.Windows.Forms.Label();
+            this.txtSenhaLogin = new System.Windows.Forms.TextBox();
+            this.txtEmailLogin = new System.Windows.Forms.TextBox();
+            this.lbCadastro = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbNomeCadastro = new System.Windows.Forms.Label();
+            this.lbTipoCadastro = new System.Windows.Forms.Label();
+            this.rdbLeitorCadastro = new System.Windows.Forms.RadioButton();
+            this.rdbColaboradorCadastro = new System.Windows.Forms.RadioButton();
+            this.txtMatriculaCadastro = new System.Windows.Forms.TextBox();
+            this.panelCadastro.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelCadastro
+            // 
+            this.panelCadastro.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(214)))), ((int)(((byte)(196)))));
+            this.panelCadastro.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelCadastro.Controls.Add(this.txtMatriculaCadastro);
+            this.panelCadastro.Controls.Add(this.rdbColaboradorCadastro);
+            this.panelCadastro.Controls.Add(this.rdbLeitorCadastro);
+            this.panelCadastro.Controls.Add(this.lbTipoCadastro);
+            this.panelCadastro.Controls.Add(this.lbNomeCadastro);
+            this.panelCadastro.Controls.Add(this.textBox1);
+            this.panelCadastro.Controls.Add(this.lbLoginAquiCadastro);
+            this.panelCadastro.Controls.Add(this.lbOULogin);
+            this.panelCadastro.Controls.Add(this.btnCadastrar);
+            this.panelCadastro.Controls.Add(this.lbEmailCadastro);
+            this.panelCadastro.Controls.Add(this.lbSenhaCadastro);
+            this.panelCadastro.Controls.Add(this.txtSenhaLogin);
+            this.panelCadastro.Controls.Add(this.txtEmailLogin);
+            this.panelCadastro.Controls.Add(this.lbCadastro);
+            this.panelCadastro.Location = new System.Drawing.Point(210, 12);
+            this.panelCadastro.Name = "panelCadastro";
+            this.panelCadastro.Size = new System.Drawing.Size(375, 361);
+            this.panelCadastro.TabIndex = 1;
+            // 
+            // lbLoginAquiCadastro
+            // 
+            this.lbLoginAquiCadastro.AutoSize = true;
+            this.lbLoginAquiCadastro.Font = new System.Drawing.Font("Gadugi", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLoginAquiCadastro.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(65)))), ((int)(((byte)(43)))));
+            this.lbLoginAquiCadastro.Location = new System.Drawing.Point(115, 332);
+            this.lbLoginAquiCadastro.Name = "lbLoginAquiCadastro";
+            this.lbLoginAquiCadastro.Size = new System.Drawing.Size(132, 14);
+            this.lbLoginAquiCadastro.TabIndex = 7;
+            this.lbLoginAquiCadastro.Text = "Faça login clicando aqui";
+            // 
+            // lbOULogin
+            // 
+            this.lbOULogin.AutoSize = true;
+            this.lbOULogin.Font = new System.Drawing.Font("Gadugi", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbOULogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(65)))), ((int)(((byte)(43)))));
+            this.lbOULogin.Location = new System.Drawing.Point(168, 318);
+            this.lbOULogin.Name = "lbOULogin";
+            this.lbOULogin.Size = new System.Drawing.Size(21, 14);
+            this.lbOULogin.TabIndex = 6;
+            this.lbOULogin.Text = "ou";
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(46)))), ((int)(((byte)(31)))));
+            this.btnCadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadastrar.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(224)))), ((int)(((byte)(212)))));
+            this.btnCadastrar.Location = new System.Drawing.Point(37, 279);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(302, 36);
+            this.btnCadastrar.TabIndex = 5;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = false;
+            // 
+            // lbEmailCadastro
+            // 
+            this.lbEmailCadastro.AutoSize = true;
+            this.lbEmailCadastro.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEmailCadastro.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(65)))), ((int)(((byte)(43)))));
+            this.lbEmailCadastro.Location = new System.Drawing.Point(33, 107);
+            this.lbEmailCadastro.Name = "lbEmailCadastro";
+            this.lbEmailCadastro.Size = new System.Drawing.Size(65, 21);
+            this.lbEmailCadastro.TabIndex = 4;
+            this.lbEmailCadastro.Text = "E-mail";
+            // 
+            // lbSenhaCadastro
+            // 
+            this.lbSenhaCadastro.AutoSize = true;
+            this.lbSenhaCadastro.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSenhaCadastro.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(65)))), ((int)(((byte)(43)))));
+            this.lbSenhaCadastro.Location = new System.Drawing.Point(33, 169);
+            this.lbSenhaCadastro.Name = "lbSenhaCadastro";
+            this.lbSenhaCadastro.Size = new System.Drawing.Size(64, 21);
+            this.lbSenhaCadastro.TabIndex = 3;
+            this.lbSenhaCadastro.Text = "Senha";
+            // 
+            // txtSenhaLogin
+            // 
+            this.txtSenhaLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(85)))), ((int)(((byte)(57)))));
+            this.txtSenhaLogin.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSenhaLogin.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Bold);
+            this.txtSenhaLogin.ForeColor = System.Drawing.Color.Transparent;
+            this.txtSenhaLogin.Location = new System.Drawing.Point(35, 196);
+            this.txtSenhaLogin.Name = "txtSenhaLogin";
+            this.txtSenhaLogin.Size = new System.Drawing.Size(302, 26);
+            this.txtSenhaLogin.TabIndex = 2;
+            this.txtSenhaLogin.Text = "•••••••••••••";
+            // 
+            // txtEmailLogin
+            // 
+            this.txtEmailLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(85)))), ((int)(((byte)(57)))));
+            this.txtEmailLogin.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtEmailLogin.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Bold);
+            this.txtEmailLogin.ForeColor = System.Drawing.Color.Transparent;
+            this.txtEmailLogin.Location = new System.Drawing.Point(37, 131);
+            this.txtEmailLogin.Name = "txtEmailLogin";
+            this.txtEmailLogin.Size = new System.Drawing.Size(302, 26);
+            this.txtEmailLogin.TabIndex = 1;
+            this.txtEmailLogin.Text = "email@dominio.com";
+            // 
+            // lbCadastro
+            // 
+            this.lbCadastro.AutoSize = true;
+            this.lbCadastro.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCadastro.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(65)))), ((int)(((byte)(43)))));
+            this.lbCadastro.Location = new System.Drawing.Point(149, 9);
+            this.lbCadastro.Name = "lbCadastro";
+            this.lbCadastro.Size = new System.Drawing.Size(89, 21);
+            this.lbCadastro.TabIndex = 0;
+            this.lbCadastro.Text = "Cadastro";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(85)))), ((int)(((byte)(57)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Bold);
+            this.textBox1.ForeColor = System.Drawing.Color.Transparent;
+            this.textBox1.Location = new System.Drawing.Point(35, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(302, 26);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "email@dominio.com";
+            // 
+            // lbNomeCadastro
+            // 
+            this.lbNomeCadastro.AutoSize = true;
+            this.lbNomeCadastro.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNomeCadastro.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(65)))), ((int)(((byte)(43)))));
+            this.lbNomeCadastro.Location = new System.Drawing.Point(33, 30);
+            this.lbNomeCadastro.Name = "lbNomeCadastro";
+            this.lbNomeCadastro.Size = new System.Drawing.Size(156, 21);
+            this.lbNomeCadastro.TabIndex = 9;
+            this.lbNomeCadastro.Text = "Nome Completo";
+            // 
+            // lbTipoCadastro
+            // 
+            this.lbTipoCadastro.AutoSize = true;
+            this.lbTipoCadastro.Font = new System.Drawing.Font("Gadugi", 10.25F, System.Drawing.FontStyle.Bold);
+            this.lbTipoCadastro.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(65)))), ((int)(((byte)(43)))));
+            this.lbTipoCadastro.Location = new System.Drawing.Point(34, 235);
+            this.lbTipoCadastro.Name = "lbTipoCadastro";
+            this.lbTipoCadastro.Size = new System.Drawing.Size(114, 18);
+            this.lbTipoCadastro.TabIndex = 10;
+            this.lbTipoCadastro.Text = "Tipo de Usuário";
+            // 
+            // rdbLeitorCadastro
+            // 
+            this.rdbLeitorCadastro.AutoSize = true;
+            this.rdbLeitorCadastro.Location = new System.Drawing.Point(37, 256);
+            this.rdbLeitorCadastro.Name = "rdbLeitorCadastro";
+            this.rdbLeitorCadastro.Size = new System.Drawing.Size(51, 17);
+            this.rdbLeitorCadastro.TabIndex = 11;
+            this.rdbLeitorCadastro.TabStop = true;
+            this.rdbLeitorCadastro.Text = "Leitor";
+            this.rdbLeitorCadastro.UseVisualStyleBackColor = true;
+            // 
+            // rdbColaboradorCadastro
+            // 
+            this.rdbColaboradorCadastro.AutoSize = true;
+            this.rdbColaboradorCadastro.Location = new System.Drawing.Point(140, 256);
+            this.rdbColaboradorCadastro.Name = "rdbColaboradorCadastro";
+            this.rdbColaboradorCadastro.Size = new System.Drawing.Size(82, 17);
+            this.rdbColaboradorCadastro.TabIndex = 12;
+            this.rdbColaboradorCadastro.TabStop = true;
+            this.rdbColaboradorCadastro.Text = "Colaborador";
+            this.rdbColaboradorCadastro.UseVisualStyleBackColor = true;
+            // 
+            // txtMatriculaCadastro
+            // 
+            this.txtMatriculaCadastro.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMatriculaCadastro.Location = new System.Drawing.Point(239, 258);
+            this.txtMatriculaCadastro.Name = "txtMatriculaCadastro";
+            this.txtMatriculaCadastro.Size = new System.Drawing.Size(98, 13);
+            this.txtMatriculaCadastro.TabIndex = 13;
+            this.txtMatriculaCadastro.Text = "Matrícula";
+            // 
+            // frmCadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(224)))), ((int)(((byte)(212)))));
+            this.ClientSize = new System.Drawing.Size(785, 385);
+            this.Controls.Add(this.panelCadastro);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmCadastro";
+            this.Text = "frmCadastro";
+            this.panelCadastro.ResumeLayout(false);
+            this.panelCadastro.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelCadastro;
+        private System.Windows.Forms.Label lbLoginAquiCadastro;
+        private System.Windows.Forms.Label lbOULogin;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Label lbEmailCadastro;
+        private System.Windows.Forms.Label lbSenhaCadastro;
+        private System.Windows.Forms.TextBox txtSenhaLogin;
+        private System.Windows.Forms.TextBox txtEmailLogin;
+        private System.Windows.Forms.Label lbCadastro;
+        private System.Windows.Forms.TextBox txtMatriculaCadastro;
+        private System.Windows.Forms.RadioButton rdbColaboradorCadastro;
+        private System.Windows.Forms.RadioButton rdbLeitorCadastro;
+        private System.Windows.Forms.Label lbTipoCadastro;
+        private System.Windows.Forms.Label lbNomeCadastro;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ frmLogin.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace sistemaEmprestimoBiblioteca
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelLogin = new System.Windows.Forms.Panel();
+            this.lbLogin = new System.Windows.Forms.Label();
+            this.txtEmailLogin = new System.Windows.Forms.TextBox();
+            this.txtSenhaLogin = new System.Windows.Forms.TextBox();
+            this.lbSenhaLogin = new System.Windows.Forms.Label();
+            this.lbEmailLogin = new System.Windows.Forms.Label();
+            this.btnLoginFrmLogin = new System.Windows.Forms.Button();
+            this.lbOULogin = new System.Windows.Forms.Label();
+            this.lbCadastreAquiLogin = new System.Windows.Forms.Label();
+            this.panelLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelLogin
+            // 
+            this.panelLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(214)))), ((int)(((byte)(196)))));
+            this.panelLogin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelLogin.Controls.Add(this.lbCadastreAquiLogin);
+            this.panelLogin.Controls.Add(this.lbOULogin);
+            this.panelLogin.Controls.Add(this.btnLoginFrmLogin);
+            this.panelLogin.Controls.Add(this.lbEmailLogin);
+            this.panelLogin.Controls.Add(this.lbSenhaLogin);
+            this.panelLogin.Controls.Add(this.txtSenhaLogin);
+            this.panelLogin.Controls.Add(this.txtEmailLogin);
+            this.panelLogin.Controls.Add(this.lbLogin);
+            this.panelLogin.Location = new System.Drawing.Point(212, 29);
+            this.panelLogin.Name = "panelLogin";
+            this.panelLogin.Size = new System.Drawing.Size(375, 320);
+            this.panelLogin.TabIndex = 0;
+            // 
+            // lbLogin
+            // 
+            this.lbLogin.AutoSize = true;
+            this.lbLogin.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(65)))), ((int)(((byte)(43)))));
+            this.lbLogin.Location = new System.Drawing.Point(150, 18);
+            this.lbLogin.Name = "lbLogin";
+            this.lbLogin.Size = new System.Drawing.Size(61, 21);
+            this.lbLogin.TabIndex = 0;
+            this.lbLogin.Text = "Login";
+            // 
+            // txtEmailLogin
+            // 
+            this.txtEmailLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(85)))), ((int)(((byte)(57)))));
+            this.txtEmailLogin.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtEmailLogin.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Bold);
+            this.txtEmailLogin.ForeColor = System.Drawing.Color.Transparent;
+            this.txtEmailLogin.Location = new System.Drawing.Point(35, 89);
+            this.txtEmailLogin.Name = "txtEmailLogin";
+            this.txtEmailLogin.Size = new System.Drawing.Size(302, 26);
+            this.txtEmailLogin.TabIndex = 1;
+            this.txtEmailLogin.Text = "email@dominio.com";
+            // 
+            // txtSenhaLogin
+            // 
+            this.txtSenhaLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(85)))), ((int)(((byte)(57)))));
+            this.txtSenhaLogin.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSenhaLogin.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Bold);
+            this.txtSenhaLogin.ForeColor = System.Drawing.Color.Transparent;
+            this.txtSenhaLogin.Location = new System.Drawing.Point(35, 166);
+            this.txtSenhaLogin.Name = "txtSenhaLogin";
+            this.txtSenhaLogin.Size = new System.Drawing.Size(302, 26);
+            this.txtSenhaLogin.TabIndex = 2;
+            this.txtSenhaLogin.Text = "•••••••••••••";
+            // 
+            // lbSenhaLogin
+            // 
+            this.lbSenhaLogin.AutoSize = true;
+            this.lbSenhaLogin.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSenhaLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(65)))), ((int)(((byte)(43)))));
+            this.lbSenhaLogin.Location = new System.Drawing.Point(31, 130);
+            this.lbSenhaLogin.Name = "lbSenhaLogin";
+            this.lbSenhaLogin.Size = new System.Drawing.Size(64, 21);
+            this.lbSenhaLogin.TabIndex = 3;
+            this.lbSenhaLogin.Text = "Senha";
+            // 
+            // lbEmailLogin
+            // 
+            this.lbEmailLogin.AutoSize = true;
+            this.lbEmailLogin.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEmailLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(65)))), ((int)(((byte)(43)))));
+            this.lbEmailLogin.Location = new System.Drawing.Point(31, 52);
+            this.lbEmailLogin.Name = "lbEmailLogin";
+            this.lbEmailLogin.Size = new System.Drawing.Size(65, 21);
+            this.lbEmailLogin.TabIndex = 4;
+            this.lbEmailLogin.Text = "E-mail";
+            // 
+            // btnLoginFrmLogin
+            // 
+            this.btnLoginFrmLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(46)))), ((int)(((byte)(31)))));
+            this.btnLoginFrmLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoginFrmLogin.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoginFrmLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(224)))), ((int)(((byte)(212)))));
+            this.btnLoginFrmLogin.Location = new System.Drawing.Point(35, 214);
+            this.btnLoginFrmLogin.Name = "btnLoginFrmLogin";
+            this.btnLoginFrmLogin.Size = new System.Drawing.Size(302, 36);
+            this.btnLoginFrmLogin.TabIndex = 5;
+            this.btnLoginFrmLogin.Text = "Login";
+            this.btnLoginFrmLogin.UseVisualStyleBackColor = false;
+            // 
+            // lbOULogin
+            // 
+            this.lbOULogin.AutoSize = true;
+            this.lbOULogin.Font = new System.Drawing.Font("Gadugi", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbOULogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(65)))), ((int)(((byte)(43)))));
+            this.lbOULogin.Location = new System.Drawing.Point(170, 263);
+            this.lbOULogin.Name = "lbOULogin";
+            this.lbOULogin.Size = new System.Drawing.Size(21, 14);
+            this.lbOULogin.TabIndex = 6;
+            this.lbOULogin.Text = "ou";
+            // 
+            // lbCadastreAquiLogin
+            // 
+            this.lbCadastreAquiLogin.AutoSize = true;
+            this.lbCadastreAquiLogin.Font = new System.Drawing.Font("Gadugi", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCadastreAquiLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(65)))), ((int)(((byte)(43)))));
+            this.lbCadastreAquiLogin.Location = new System.Drawing.Point(113, 286);
+            this.lbCadastreAquiLogin.Name = "lbCadastreAquiLogin";
+            this.lbCadastreAquiLogin.Size = new System.Drawing.Size(139, 14);
+            this.lbCadastreAquiLogin.TabIndex = 7;
+            this.lbCadastreAquiLogin.Text = "Cadastre-se clicando aqui";
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(224)))), ((int)(((byte)(212)))));
+            this.ClientSize = new System.Drawing.Size(785, 385);
+            this.Controls.Add(this.panelLogin);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmLogin";
+            this.Text = "frmLogin";
+            this.panelLogin.ResumeLayout(false);
+            this.panelLogin.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelLogin;
+        private System.Windows.Forms.TextBox txtEmailLogin;
+        private System.Windows.Forms.Label lbLogin;
+        private System.Windows.Forms.TextBox txtSenhaLogin;
+        private System.Windows.Forms.Button btnLoginFrmLogin;
+        private System.Windows.Forms.Label lbEmailLogin;
+        private System.Windows.Forms.Label lbSenhaLogin;
+        private System.Windows.Forms.Label lbCadastreAquiLogin;
+        private System.Windows.Forms.Label lbOULogin;
+    }
+}
